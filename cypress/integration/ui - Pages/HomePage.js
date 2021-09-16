ad0609236708806cd635d440e433827420cf2de3
SofiaOlivera <olivera.sofia@gmail.com>
2021-09-16 16:46:10 -0300

    Add: Xpath for cypress on UI Test

--- cypress/integration/ui - Pages/HomePage.js
+++ cypress/integration/ui - Pages/HomePage.js
@@ -14,7 +14,7 @@ const LIST = {
   LIST_RESULT_ITEM_TWO : '[data-image-index=3]', 
   LIST_RESULT_ITEM_FIVE : '[data-image-index=5]',
   LIST_RESULT_ITEM_SEVENTEEN : '[data-image-index=17]',
-  //npm install --save-dev cypress@8.3.1
+  LIST_SEARCH_ITEMS_XPATH : '//*[@class="a-size-medium a-color-base a-text-normal"]'
 };
 
 const ITEM = {
@@ -58,13 +58,28 @@ describe('Load Page', () => {
     
   })
 
-  it(' US2: expect matches delivery Location', () => {
-
-    cy.get(SEARCH.MAIN_SEARCH_BAR).type('doll')
+  it('US2 Select last items by xpath', () =>{
+    cy.get(SEARCH.MAIN_SEARCH_BAR).type('dragon')
     cy.wait(500)
     cy.get(SEARCH.MAIN_SEARCH_BAR).click()
     cy.get(SEARCH.MAIN_SEARCH_BUTTON).click()
-    cy.get(LIST.LIST_RESULT_ITEM_TWO).click()
+    cy.xpath('//*[@class="a-size-medium a-color-base a-text-normal"]')
+     // .xpath('')
+      .last()
+      .click()
+
+  })
+
+  it('US3: expect matches delivery Location', () => {
+    
+    cy.get(SEARCH.MAIN_SEARCH_BAR).click()
+    cy.get(SEARCH.MAIN_SEARCH_BAR).type('doll')
+    cy.wait(500)
+    cy.get(SEARCH.MAIN_SEARCH_BUTTON).click()
+    cy.xpath(LIST.LIST_SEARCH_ITEMS_XPATH)
+    // .xpath('')
+     .first()
+     .click()
 
     const normalizeText = (inputString) => inputString.replace(/\s/g, '').toLowerCase()
 
@@ -85,17 +100,20 @@ describe('Load Page', () => {
     })
 
 
-    it('US3: Add to cart item if it is avilable if not go back', () => {
+    it('US4: Add to cart item if it is avilable if not go back', () => {
       cy.get(SEARCH.MAIN_SEARCH_BAR).type('dragon')
       cy.wait(500)
       cy.get(SEARCH.MAIN_SEARCH_BAR).click()
       cy.get(SEARCH.MAIN_SEARCH_BUTTON).click()
-      cy.get(LIST.LIST_RESULT_ITEM_TWO).click()
+      cy.xpath(LIST.LIST_SEARCH_ITEMS_XPATH)
+      // .xpath('')
+       .last()
+       .click()
 
       cy.get("body").then($body => {
-        if ($body.find('#add-to-cart-button').length > 0) {   
+        if ($body.find(ITEM.ADD_TO_CART_BUTTON).length > 0) {   
             //evaluates as true
-            cy.get('#add-to-cart-button').click()
+            cy.get(ITEM.ADD_TO_CART_BUTTON).click()
         } else {
           cy.go('back')
         }
@@ -104,9 +122,6 @@ describe('Load Page', () => {
 
     })
 
-
-      
-    
 
 
 })
